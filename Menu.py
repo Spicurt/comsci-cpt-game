33d0f51478890d07fb0346e0e92a9926b8db939f
Domino-wolf <125303827+Domino-wolf@users.noreply.github.com>
2024-05-30 13:42:46 -0400

    almost completed menu. Intend to add art and change button positions

--- Menu.py
+++ Menu.py
@@ -1,64 +1,87 @@
-#https://www.youtube.com/watch?v=2iyx8_elcYg&ab_channel=CodingWithRuss
 import pygame
+import button
 
 pygame.init()
-
-WIDTH = 640
-HEIGHT = 480
-SIZE = (WIDTH, HEIGHT)
-
-screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
-clock = pygame.time.Clock()
-pygame.display.set_caption("Main Menu")
 
-#Game variable:
+#create game window
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#game variables
 game_paused = False
+menu_state = "main"
 
-#Drawing text
+#define fonts
 font = pygame.font.SysFont("arialblack", 40)
 
+#define colours
 TEXT_COL = (255, 255, 255)
 
+#load images
+resume_img = pygame.image.load("Sprites/resume.png").convert_alpha()
+options_img = pygame.image.load("Sprites/menu.png").convert_alpha()
+quit_img = pygame.image.load("Sprites/exit.png").convert_alpha()
+video_img = pygame.image.load('Sprites/video.png').convert_alpha()
+audio_img = pygame.image.load('Sprites/audio.png').convert_alpha()
+keys_img = pygame.image.load('Sprites/keys.png').convert_alpha()
+back_img = pygame.image.load('Sprites/back.png').convert_alpha()
+
+#create button instances
+resume_button = button.Button(304, 125, resume_img, 1)
+options_button = button.Button(297, 250, options_img, 1)
+quit_button = button.Button(336, 375, quit_img, 1)
+video_button = button.Button(226, 75, video_img, 1)
+audio_button = button.Button(225, 200, audio_img, 1)
+keys_button = button.Button(246, 325, keys_img, 1)
+back_button = button.Button(332, 450, back_img, 1)
+
 def draw_text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    screen.blit(img, (x,y))
-# ---------------------------
-# Initialize global variables
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
 
+#game loop
+run = True
+while run:
 
-# ---------------------------
+  screen.fill((52, 78, 91))
 
-running = True
-while running:
-    # EVENT HANDLING
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                game_paused = True
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
+  #check if game is paused
+  if game_paused == True:
+    #check menu state
+    if menu_state == "main":
+      #draw pause screen buttons
+      if resume_button.draw(screen):
+        game_paused = False
+      if options_button.draw(screen):
+        menu_state = "options"
+      if quit_button.draw(screen):
+        run = False
+    #check if the options menu is open
+    if menu_state == "options":
+      #draw the different options buttons
+      if video_button.draw(screen):
+        print("Video Settings")
+      if audio_button.draw(screen):
+        print("Audio Settings")
+      if keys_button.draw(screen):
+        print("Change Key Bindings")
+      if back_button.draw(screen):
+        menu_state = "main"
+  else:
+    draw_text("Press SPACE to pause", font, TEXT_COL, 160, 250)
 
-    # GAME STATE UPDATES
-    # All game math and comparisons happen here
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_SPACE:
+        game_paused = True
+    if event.type == pygame.QUIT:
+      run = False
 
-    # DRAWING
-    screen.fill((52, 78, 91))  # always the first drawing command
-
-    #checking if game is paused or no
-    if game_paused == True:
-        pass
-        #display menu
-    else:
-        draw_text("Press SPACE to pause", font, TEXT_COL, 71, 211)
-
-    # Must be the last two lines
-    # of the game loop
-    pygame.display.update()
-    clock.tick(30)
-    #---------------------------
-
+  pygame.display.update()
+  clock.tick(30)
 
 pygame.quit()
